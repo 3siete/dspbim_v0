8700c2295b62594ea62ee745277fa12372fc0cf6
coque <mateogabus@gmail.com>
2024-05-03 03:53:22 -0300

    feat:

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -17,16 +17,21 @@ export class AboutComponent implements OnInit, AfterViewInit {
       '.informacion-adicional'
     ) as HTMLElement;
     const verMas = tarjeta.querySelector('.ver-mas') as HTMLElement;
+    const imagen = tarjeta.querySelector(
+      '.contenedor-imagen img'
+    ) as HTMLImageElement;
 
     if (
       infoAdicional.style.maxHeight &&
       infoAdicional.style.maxHeight !== '0px'
     ) {
-      infoAdicional.style.maxHeight = '0px'; // Asigna "0px" en lugar de null
+      infoAdicional.style.maxHeight = '0px'; // Oculta la información adicional
       verMas.textContent = 'Ver más';
+      imagen.style.transform = 'scale(1)'; // Tamaño original de la imagen
     } else {
-      infoAdicional.style.maxHeight = `${infoAdicional.scrollHeight}px`;
+      infoAdicional.style.maxHeight = `${infoAdicional.scrollHeight}px`; // Muestra la información adicional
       verMas.textContent = 'Ver menos';
+      imagen.style.transform = 'scale(1.1)'; // Aumenta el tamaño de la imagen
     }
   }
 }
