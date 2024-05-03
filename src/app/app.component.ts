a471bce728247ec59a3ed39c0e16d8e4511a20a7
coque <mateogabus@gmail.com>
2024-05-03 19:00:52 -0300

    feat:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterOutlet, Router, NavigationStart } from '@angular/router';
 import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { isPlatformBrowser, ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,18 @@ import { RouterModule, Routes } from '@angular/router';
 })
 export class AppComponent {
   title = 'dspbim_v0';
+  constructor(
+    private router: Router,
+    private viewportScroller: ViewportScroller,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.router.events.subscribe((event) => {
+      if (
+        event instanceof NavigationStart &&
+        isPlatformBrowser(this.platformId)
+      ) {
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
 }
